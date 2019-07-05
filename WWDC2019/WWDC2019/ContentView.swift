772e3ca225aab116145d5b93a3138b493aa915d0
unnnyong <unnnyong@gmail.com>
2019-07-05 23:23:39 +0900

    creat xcode file@

---
+++ WWDC2019/WWDC2019/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  WWDC2019
+//
+//  Created by Eunyeong Kim on 2019/07/05.
+//  Copyright © 2019 Eunyeong Kim. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
